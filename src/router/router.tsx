b126da97ae20b183355eaa40ad84d9a51d62dcc7
devin <1076971426@qq.com>
2019-09-28 00:07:01 +0800

    config

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { BrowserRouter as Router, Route ,Switch } from 'react-router-dom';
+import { HashRouter as Router, Route ,Switch } from 'react-router-dom';
 import Loadable from 'react-loadable';
 import Loading from '../components/Loading';
 
